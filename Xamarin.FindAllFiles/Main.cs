561456c64c17194b389fdc7fa4c9b203e12fcc85
Sandy Armstrong <sandy@xamarin.com>
2019-11-17 09:12:25 -0800

    Initial commit

---
+++ Xamarin.FindAllFiles/Main.cs
@@ -0,0 +1,14 @@
+﻿// Main.cs
+using AppKit;
+
+namespace Xamarin.FindAllFiles
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
