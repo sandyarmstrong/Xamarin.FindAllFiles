dcca70681622dae9fd36ca7e5fc0d48231ea5d97
Sandy Armstrong <sandy@xamarin.com>
2019-11-21 01:02:06 -0500

    Basic engine

---
+++ Xamarin.RipGrep/FindOptions.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace Xamarin.RipGrep
+{
+    public class FindOptions
+    {
+        public string Query { get; set; }
+
+        public string WorkingDirectory { get; set; }
+
+        public bool CaseSensitive { get; set; }
+
+        public bool IgnoreCase { get; set; }
+
+        public bool NoIgnore { get; set; }
+
+        public bool NoIgnoreParent { get; set; }
+
+        public bool NoConfig { get; set; }
+
+        public long? MaxFileSize { get; set; }
+
+        public bool CrlfSupport { get; set; }
+
+        public IReadOnlyList<string> ToArgumentList()
+        {
+            var args = new List<string>
+            {
+                $"\"{Query}\"",
+                "--json"
+            };
+
+            if (NoConfig)
+                args.Add("--no-config");
+
+            if (CrlfSupport)
+                args.Add("--crlf");
+
+            if (MaxFileSize.HasValue)
+            {
+                args.Add("--max-filesize");
+                args.Add(MaxFileSize.Value.ToString());
+            }
+
+            if (CaseSensitive)
+                args.Add("--case-sensitive");
+
+            if (IgnoreCase)
+                args.Add("--ignore-case");
+
+            if (NoIgnore)
+                args.Add("--no-ignore");
+
+            if (NoIgnoreParent)
+                args.Add("--no-ignore-parent");
+
+            return args;
+        }
+
+        public override string ToString()
+            => string.Join(' ', ToArgumentList());
+    }
+}
--- Xamarin.RipGrep/RipGrepEngine.cs
+++ Xamarin.RipGrep/RipGrepEngine.cs
@@ -1,8 +1,68 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+//using Xamarin.ProcessControl;
 
 namespace Xamarin.RipGrep
 {
     public class RipGrepEngine
     {
+        public Task SearchAsync(
+            FindOptions options,
+            Action<Message> messageHandler,
+            CancellationToken cancellationToken)
+        {
+            // TODO: Nope, Exec doesn't support timeouts or cancellation/killing of any kind
+            //await Exec.RunAsync(
+            //    ExecFlags.RedirectStdout,
+            //    HandleOutputSegment,
+            //    "/usr/local/bin/rg",
+            //    options.ToArgumentList().ToArray());
+
+            //void HandleOutputSegment(ConsoleRedirection.Segment segment)
+            //{
+            //}
+
+            var p = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/usr/local/bin/rg",
+                    WorkingDirectory = String.IsNullOrEmpty(options.WorkingDirectory) ? Environment.CurrentDirectory : options.WorkingDirectory,
+                    Arguments = options.ToString(),
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    RedirectStandardOutput = true,
+                }
+            };
+
+            return Task.Run(() =>
+            {
+                cancellationToken.Register(() =>
+                {
+                    p.OutputDataReceived -= HandleOutputDataReceived;
+                    p.Kill();
+
+                });
+
+                p.OutputDataReceived += HandleOutputDataReceived;
+                p.Start();
+                p.BeginOutputReadLine();
+                p.WaitForExit();
+
+                void HandleOutputDataReceived(object o, DataReceivedEventArgs e)
+                {
+                    // TODO: try/catch here, maybe elsewhere, maybe put exceptions on Task object
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        var message = JsonSerializer.Deserialize<Message>(e.Data);
+                        messageHandler(message);
+                    }
+                }
+            }, cancellationToken);
+        }
     }
 }
